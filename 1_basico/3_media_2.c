0fe6133af23557626c08320baf6b4876835e81c4
Yuri-Sanchez <yuri.tecnologia@outlook.com>
2025-04-26 16:50:12 -0300

    adicionado funcao, vetores e estrutura de repeticao

--- 1_basico/3_media_2.c
+++ 1_basico/3_media_2.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <locale.h>
+
+float calcular_media(float notas[]){
+    float pesos[3] = {2, 3, 5};
+    float soma_notas = 0, soma_pesos = 0;
+
+    for(int i = 0;i < 3; i++){
+        soma_notas += notas[i] * pesos[i];
+        soma_pesos += pesos[i];
+    }
+    return soma_notas / soma_pesos;
+}
 
 int main(){
-    float A,B,C,MEDIA;
-    scanf("%f",&A);
-    scanf("%f",&B);
-    scanf("%f",&C);
-    MEDIA=(A*2+B*3+C*5)/10;
-    printf("MEDIA = %.1f\n",MEDIA);
+    setlocale(LC_ALL, "Portuguese");
+
+    float media, notas[3];
+
+    for(int i = 0; i < 3; i++){
+        printf("Digite a %d° nota: \n", i + 1);
+        scanf("%f", &notas[i]);
+    }
+
+    media = calcular_media(notas);
+    printf("Média = %.1f\n", media);
+
     return 0;
 }
