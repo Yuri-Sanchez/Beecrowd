b9cbc4981fdadc1dcecdd4ee5a1f3fa9e5b3dc8f
Yuri-Sanchez <yuri.tecnologia@outlook.com>
2025-04-26 17:11:31 -0300

    adicionado funcao, vetor e repeticao

--- 1_basico/4_diferenca.c
+++ 1_basico/4_diferenca.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <locale.h>
+
+int calcular_sub(int valor[]){
+   return (valor[0] * valor[1]) - (valor[2]* valor[3]);
+}
 
 int main(){
-    int A,B,C,D,DIFERENCA;
-    scanf("%d",&A);
-    scanf("%d",&B);
-    scanf("%d",&C);
-    scanf("%d",&D);
-    DIFERENCA=(A*B-C*D);
-    printf("DIFERENCA = %d\n",DIFERENCA);
+    setlocale(LC_ALL, "Portuguese");
+
+    int valor[4], diferenca;
+
+    for(int i = 0; i < 4; i++){
+        printf("Digite o %d valor: \n", i + 1);
+        scanf("%d", &valor[i]);
+    }
+
+    diferenca = calcular_sub(valor);
+    
+    printf("Diferenca = %d\n",diferenca);
     return 0;
 }
