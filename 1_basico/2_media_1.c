9e36ce9a1a64f7a019873db57e03ffd80399d4ca
Yuri-Sanchez <yuri.tecnologia@outlook.com>
2025-04-26 14:43:18 -0300

    adicionado funcao e estrutura de repeticao

--- 1_basico/2_media_1.c
+++ 1_basico/2_media_1.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <locale.h>
+
+float calcular_media(float notas[], float tamanho){
+    float soma = 0;
+    for(int i = 0; i < 2; i++){
+        soma += notas[i];
+    }
+    return soma / tamanho;
+}
  
 int main() {
-    float A,B,media;
-    scanf("%f",&A);
-    scanf("%f",&B);
-    media=(A* 3.5 + B * 7.5)/11;
+    setlocale(LC_ALL, "Portuguese");
+
+    float n[2], media;
+    int i;
+
+    for(i=0;i<2;i++){
+        printf("Digite a %d° nota: \n", i + 1);
+        scanf("%f",&n[i]);
+    }
+    
+    media = calcular_media(n,2);
+    
     printf("MEDIA = %.5f",media);
+    
     return 0;
 }
