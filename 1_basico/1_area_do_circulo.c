dd1e420904caa049186fa0ff0c4d0855b637fa9b
Yuri-Sanchez <yuri.tecnologia@outlook.com>
2025-04-26 14:42:06 -0300

    adiciona funcao

--- 1_basico/1_area_do_circulo.c
+++ 1_basico/1_area_do_circulo.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <locale.h>
+
+double calcular_area(double pi, double raio){
+    return pi * raio * raio;
+}
+
 int main() {
+    setlocale(LC_ALL, "Portuguese");
+
     double R, A, n = 3.14159;
     
+    printf("Digite o raio do circulo para saber a área: \n");
     scanf("%lf", &R);
-    A = n * R * R;
+
+    A = calcular_area(n,R);
+
     printf("A= %lf\n", A);
     return 0;
 }
